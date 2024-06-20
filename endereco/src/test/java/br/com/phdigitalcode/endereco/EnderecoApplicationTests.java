ce6f5b4e5e3a3242112716026a21526fe31d9062
phelippnd <phelippnd@gmail.com>
2024-06-20 02:24:42 +0000

    Pending changes exported from your codespace

---
+++ endereco/src/test/java/br/com/phdigitalcode/endereco/EnderecoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.phdigitalcode.endereco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnderecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
